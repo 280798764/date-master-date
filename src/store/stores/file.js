aa47c923a7220bfd2ac33c62ac9c9f509f7df57b
wangyanyan <tinasunny@163.com>
2019-03-18 10:37:34 +0800

    配置url

--- src/store/stores/file.js
+++ src/store/stores/file.js
@@ -5,7 +5,7 @@ const actions = {
   'a:file/fileUpload' ({commit}, {file, cmd}) {
     return ajax.apiFileUpload(file, cmd)
   },
-  'a:file/fileUploadToQiNiu' ({commit}, file) {
+  'a:file/fileUploadToQiNiu' ({commit}, file) { // 系统大类和系统小类的上传在使用
     return ajax.apiUploadQiNiu2(file)
   }
 }
--- src/utils/config.js
+++ src/utils/config.js
@@ -5,30 +5,46 @@ const HOST = window.location.host
 
 const config = {
   'prod': {
-    origin: '//wms.isesol.com',
+    // 个性化后端域名
+    service: '//service.isesol.com',
+    // 产能旧版域名
+    apiCN: '//cnservice.isesol.com',
+    origin: '//imedata.isesol.com',
     api: '//api.isesol.com',
-    fileFTP: '//oncall.isesol.com/oncallapi',
+    // fileFTP: '//oncall.isesol.com/oncallapi',
     qiNiu: '//upload.qbox.me/',
     uploadPath: '//imedata.isesol.com' // 上传文件 导入
   },
   'pre': {
-    origin: '//wms.preisesol.com',
+    // 个性化后端域名
+    service: '//service.preisesol.com',
+    // 产能旧版域名
+    apiCN: '//cnservice.preisesol.com',
+    origin: '//imedata.preisesol.com',
     api: '//api.preisesol.com',
-    fileFTP: '//oncall.preisesol.com/oncallapi',
+    // fileFTP: '//oncall.preisesol.com/oncallapi',
     qiNiu: '//upload.qbox.me/',
     uploadPath: '//imedata.preisesol.com' // 上传文件 导入
   },
   'test': {
-    origin: '//wms.i5sesol.com',
+    // 个性化后端域名
+    service: '//service.i5sesol.com',
+    // 产能旧版域名
+    apiCN: '//cnservice.i5sesol.com',
+    origin: '//imedata.i5sesol.com',
     api: '//api.i5sesol.com',
-    fileFTP: '//oncall.i5sesol.com/oncallapi',
+    // fileFTP: '//oncall.i5sesol.com/oncallapi',
     qiNiu: '//upload.qbox.me/',
     uploadPath: '//imedata.i5sesol.com' // 上传文件 导入
   },
   'dev': {
-    origin: '//wms.isesoldev.com',
-    api: '//api.isesoldev.com',
-    fileFTP: '//oncall.isesoldev.com/oncallapi',
+    // 个性化后端域名
+    service: 'http://service.isesoldev.com',
+    // 产能旧版域名
+    apiCN: 'http://cnservice.isesoldev.com',
+    origin: '//imedata.isesoldev.com',
+    api: 'http://api.isesoldev.com',
+    // fileFTP: '//oncall.isesoldev.com/oncallapi',
     qiNiu: '//upload.qiniu.com/',
     uploadPath: '//imedata.isesoldev.com' // 上传文件 导入
   }
@@ -49,7 +65,15 @@ function createDomain (host) {
     DOMAIN = config['dev']
   }
   DOMAIN.tld = topDomain
+  // DOMAIN.apiPath = DOMAIN.api + '/cgi'
+  // 个性化后端接口
+  DOMAIN.apiPathUncode = DOMAIN.service + '/B2C/services/invokeSrv/isesolService'
+  // 个性化验证码获取
+  DOMAIN.imgPath = DOMAIN.service + '/B2C/services/invokeSrv/verifyImage'
+  // 标准CGI接口
   DOMAIN.apiPath = DOMAIN.api + '/cgi'
+  // 产能旧版接口
+  DOMAIN.apiCNOldPath = DOMAIN.apiCN + '/factorycenter/fcService'
   return DOMAIN
 }
 
